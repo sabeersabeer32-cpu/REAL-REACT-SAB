1ecff7ba0227a84f81fc5ea6fd088a4bcc0e5586
A SABEER <sabeersabeer32@gmail.com>
2026-02-11 10:19:57 -0800

    one new component added in component folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {HELLO} from './components/component1'
+ 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+   
       <div>
+      <HELLO />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,5 +35,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
---
+++ src/components/component1.jsx
@@ -0,0 +1,11 @@
+
+function HELLO()
+{
+    return(
+        <div>
+            <h1>hai world</h1>
+        </div>
+    )
+}
+
+export default HELLO;
